fadd2042539ffc58b925837f16859c217a9ed1e9
dejavudwh <494319000@qq.com>
2020-01-25 19:51:24 +0800

    Updated symbol table data structure

--- analysis/src/local_resolver.rs
+++ analysis/src/local_resolver.rs
@@ -1,11 +1,20 @@
-pub struct TopLevelScope {
+use std::collections::HashMap;
+use crate::parse::ast:: {
+    ProgramNode,
+}
+use crate::parse::DefNode;
 
+pub struct TopLevelScope {
+    pub global_var_map: HashMap<String, DefNode>,
+    pub func_map: HashMap<String, DefNode>,
+    pub scopes: Vec<LocalScope>,
 }
 
 pub struct LocalScope {
-
+    pub var_map: HashMap<String, DefNode>,
+    pub scopes: Vec<LocalScope>,
 }
 
-pub struct LocalResolver {
-
+pub fn local_resolver(ast: ProgramNode) {
+    
 }
